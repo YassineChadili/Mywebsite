61b1df9b5e85565e9cc3905968fb4dff9e32e6d0
YassineChadili <yassinechadili12@outlook.com>
2023-09-13 17:58:30 +0200

    About me page and the connections with the others pages

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,51 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Welkom op mijn portfolio site!') }}
         </h2>
+        <style>
+            .dropbtn {
+              background-color: black;
+              color: white;
+              padding: 11px;
+              font-size: 16px;
+              border: none;
+              cursor: pointer;
+            }
+            .dropdown {
+              position: relative;
+              display: inline-block;
+            }
+            .dropdown-content {
+              display: none;
+              position: absolute;
+              background-color: #f9f9f9;
+              min-width: 100px;
+              box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+              z-index: 1;
+            }
+            .dropdown-content a {
+              color: black;
+              padding: 12px 16px;
+              text-decoration: none;
+              display: block;
+            }
+            .dropdown-content a:hover {background-color: #f1f1f1}
+
+            .dropdown:hover .dropdown-content {
+              display: block;
+            }
+            .dropdown:hover .dropbtn {
+              background-color: black;
+            }
+            </style>
+        <div class="dropdown">
+            <button class="dropbtn">Menu</button>
+            <div class="dropdown-content">
+              <a href="/">Homepagina</a>
+              <a href="/aboutme">About me</a>
+            </div>
+          </div>
     </x-slot>
 
     <div class="py-12">
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/aboutme', function () {
+    return view('aboutme');
+})->middleware(['auth', 'verified'])->name('aboutme');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
